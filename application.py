65dd7fca6c3a7b0e7aa0089a4322081fb47fc855
Meril Abraham <merilonline@gmail.com>
2018-10-29 21:43:42 +0530

    Adding Flask-ask

--- application.py
+++ application.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from flask_ask import Ask,question,statement,session
 import random
 
 app = Flask(__name__)
+ask = Ask(app, '/')
+
+
 
 @app.route("/")
 def hello():
@@ -11,3 +15,9 @@ def hello():
 def rand():
     foo = ['its beautiful', 'its hot', 'its good', 'its calm', 'its peaceful']
     return random.choice(foo)
+
+@ask.intent('GetNewFactIntent')
+def intent():
+    foo = ['its beautiful', 'its hot', 'its good', 'its calm', 'its peaceful']
+    text = 'Hello There, '+random.choice(foo)
+    return statement(text)
