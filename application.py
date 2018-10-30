6912ac56bfe14a7fb053230a7414e49a782940ac
Meril Abraham <merilonline@gmail.com>
2018-10-30 15:05:49 +0530

    Adding Flask-ask

--- application.py
+++ application.py
@@ -6,23 +6,17 @@ app = Flask(__name__)
 ask = Ask(app, '/')
 
 
-
 @app.route("/hello")
 def hello():
     return "Hello World!"
 
-@app.route("/fact")
-def rand():
-    foo = ['its beautiful', 'its hot', 'its good', 'its calm', 'its peaceful']
-    return random.choice(foo)
-
 @ask.launch
 def start_skill():
-    welcome_message = 'Hello there,'
+    welcome_message = 'Hello'
     return statement(welcome_message)
 
 @ask.intent('GetNewFactIntent')
 def intent():
-    foo = ['Thiruvalla is a beautiful city', 'Thiruvalla is a very developed city', 'Thiruvalla is good', 'Thiruvalla is very calm','Thiruvalla is a peaceful city']
-    text = 'Hello There, '+random.choice(foo)
+    foo = ['Tiruvalla is located in Pathanamthitta district in the State of Kerala in South India.','The town is spread over an area of 27.94 square km','Tiruvalla is the biggest commercial centre in the district of Pathanamthitta','Tiruvalla lies on the banks of the rivers Manimala and Pamba','Tiruvalla is a land-locked region surrounded by irrigating streams and rivers','Tiruvalla is regarded as the "cultural capital of Central Travancore"','Tiruvalla is called "Land of non resident Indians']
+    text = 'Hello, '+random.choice(foo)
     return statement(text)
